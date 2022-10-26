7e745dac54fa600c7bf2d2dbc39f95d3f6edf121
Pedro Henrique Almeida Lima <almeidaph13@gmail.com>
2022-10-26 08:43:59 -0300

    Add files via upload

--- CursoCSharp/Program.cs
+++ CursoCSharp/Program.cs
@@ -2,18 +2,47 @@
 using System.Collections.Generic;
 
 using CursoCSharp.Fundamentos;
+using CursoCSharp.EstruturaDeControle;
+using CursoCSharp.ClassesEMetodos;
+using CursoCSharp.Colecoes;
+using CursoCSharp.OO;
 
 namespace CursoCSharp {
     class Program {
         static void Main(string[] args) {
             var central = new CentralDeExercicios(new Dictionary<string, Action>() {
+                //fundamentos
                 {"Primeiro Programa - Fundamentos", PrimeiroPrograma.Executar},
                 {"Comentários - Fundamentos", Comentarios.Executar },
                 {"Variáveis e Constantes - Fundamentos", VariaveisEConstantes.Executar },
                 {"Inferência - Fundamentos", Inferencia.Executar },
                 {"Interpolação - Fundamentos", Interpolacao.Executar },
+                {"Lendo Dados - Fundamentos", LendoDados.Executar },
+                {"a Dados - Fundamentos", OperadorTernario.Executar },
+                //estrutura de controle
+                {"Estrutura If - Estrutura de controle", EstruturaIf.Executar },
+                //Classes e metodos
+                {"Membros - Classes e Metodos", Membros.Executar },
+                {"Construtores - Classes e Metodos", Construtores.Executar },
+                {"Metodos Com Retorno - Classes e Metodos", MetodosComRetorno.Executar },
+                {"Get Set - Classes e Metodos", GetSet.Executar },
+                {"Enum - Classes e Metodos", ExemploEnum.Executar },
+                {"Struct - Classes e Metodos", ExemploStruct.Executar },
+                {"StructVsClasse - Classes e Metodos", StructVsClasse.Executar },
+                //Coleções
+                {"Arrays - Coleções", Colecoes.Array.Executar },
+                {"Coleções List - Coleções", ColecoesList.Executar },
+                {"Coleções Array List - Coleções", ColecoesArrayList.Executar },
+                {"Set - Coleções", ColecoesSet.Executar },
+                {"Queue - Coleções", ColecoesQueue.Executar },
+                {"Igualdade - Coleções", Igualdade.Executar },
+                {"Stack - Coleções", ColecoesStack.Executar },
+                {"Dictionary - Coleções", ColecoesDictionary.Executar },
+                //OO
+                {"Herança - OO", Heranca.Executar },
 
             });
+
 
             central.SelecionarEExecutar();
         }
